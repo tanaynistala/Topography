b72f65db4c9762cc404445e63ceece7a36e1fd0f
Tanay Nistala <tanay.nistala@tufts.edu>
2024-06-02 07:27:20 -0400

    Merged in centroid data

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
 import { readdir } from "node:fs/promises";
+import { $ } from "bun";
 
 // read all the files in the current directory
 const countryCodes = await readdir(`./topography`);
+
+const centroidsFile = Bun.file("./geography/centroids.geo.json");
+const centroids = await centroidsFile.json();
 
 countryCodes.forEach(async (code) => {
     console.log(code);
@@ -9,6 +13,12 @@ countryCodes.forEach(async (code) => {
     const baseFile = Bun.file(`./topography/${code}/${code}_adm0.topo.json`);
     const json = await baseFile.json();
     const transform = json.transform;
+
+    const code2 = json.objects.states.geometries[0].properties.ISO2
+    const features: any[] = centroids.features;
+    const feature = features.find((f) => f.properties.ISO === code2)
+    transform.translate = feature.geometry.coordinates;
+
 
     const files = await readdir(`./topography/${code}`);
     files.forEach(async (fileName) => {
@@ -21,3 +31,5 @@ countryCodes.forEach(async (code) => {
         Bun.write(file, contents);
     })
 })
+
+await $`bunx prettier --write "./topography/*/*.json"`
