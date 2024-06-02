0366e7f2652d387dcf02e511c1891692acef7d30
Tanay Nistala <tanay.nistala@tufts.edu>
2024-06-02 08:10:56 -0400

    Fixed centroid/translate issue

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
 import { readdir } from "node:fs/promises";
+import { $ } from "bun";
 
 // read all the files in the current directory
 const countryCodes = await readdir(`./topography`);
+
+const centroidsFile = Bun.file("./geography/centroids.geo.json");
+const centroids = await centroidsFile.json();
 
 countryCodes.forEach(async (code) => {
     console.log(code);
@@ -10,14 +14,25 @@ countryCodes.forEach(async (code) => {
     const json = await baseFile.json();
     const transform = json.transform;
 
+    const code2 = json.objects.states.geometries[0].properties.ISO2
+    const features: any[] = centroids.features;
+    const feature = features.find((f) => f.properties.ISO === code2)
+    const coordinates = feature.geometry.coordinates;
+
     const files = await readdir(`./topography/${code}`);
     files.forEach(async (fileName) => {
         const file = Bun.file(`./topography/${code}/${fileName}`);
         const json = await file.json();
         json.transform = transform;
+        if (fileName === `${code}_adm0.topo.json`) {
+            json.objects.states.geometries[0].properties.centroid = coordinates;
+        }
+
         const contents = JSON.stringify(json);
 
         console.log("Writing", fileName);
         Bun.write(file, contents);
     })
 })
+
+await $`bunx prettier --write "./topography/*/*.json"`
